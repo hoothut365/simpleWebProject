f1d6c014159d32d0de9200d425deeff46116f6d0
hoothut365 <84186715+hoothut365@users.noreply.github.com>
2021-05-14 17:35:10 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -1,13 +1,17 @@
 function getPriceData(){
-  const pricesWs = new WebSocket('wss://ws.coincap.io/prices?assets=bitcoin,ethereum')
+  const pricesWs = new WebSocket('wss://ws.coincap.io/prices?assets=bitcoin,ethereum,cardano,dogecoin')
 
   pricesWs.onmessage = function (msg) {
     let data = JSON.parse(msg.data);
-    
+    //console.log(data);
     if(data.bitcoin!=undefined)
       document.getElementById("btc").innerHTML = "  $"+data.bitcoin;
     if(data.ethereum!=undefined)
       document.getElementById("eth").innerHTML = "  $"+data.ethereum;
+    if(data.cardano!=undefined)
+      document.getElementById("ada").innerHTML = "  $"+data.cardano;
+    if(data.dogecoin!=undefined)
+      document.getElementById("doge").innerHTML = "  $"+data.dogecoin;
   }
 
 }
